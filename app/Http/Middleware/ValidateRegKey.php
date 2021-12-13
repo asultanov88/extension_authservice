774023c3162a92eeb66e9dc8dbe48ba9b4867473
asultanov88 <uzsultanov@gmail.com>
2021-12-13 16:43:11 -0500

    check for wxpired user.

--- app/Http/Middleware/ValidateRegKey.php
+++ app/Http/Middleware/ValidateRegKey.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use App\Models\ClientAuth;
+use Carbon\Carbon;
 
 class ValidateRegKey
 {
@@ -25,7 +26,14 @@ class ValidateRegKey
         $client = ClientAuth::where('AuthKey', '=', $request['RegistrationKey'])->first();
 
         if($client){
+
+            // Checks if client subscription is expired.
+            if($client['ExpirationDate'] < Carbon::now()){
+                return response()->
+                json(['error' => 'Expired Client'], 401);            }
+
             return $next($request);
+
         }else{
             return response()->
             json(['error' => 'Unauthorized'], 401);
